9a95b6275369f56b6707efa0245b8e6d2cd3b068
Frank Pfattheicher <fpf@ict-baden.de>
2025-06-02 20:08:17 +0200

    WIP: Install cmake 3.16

--- build-linux.sh
+++ build-linux.sh
@@ -62,8 +62,11 @@ echo " CMake pjsua2.linux x64"
 cp pjproject/pjlib/include/pj/limits.h pjproject/pjlib/include/pj/compat/limits.h
 echo "************************"
 cd pjsua2.linux
-echo cmake --configure .
-cmake --build .
+wget https://cmake.org/files/v3.16/cmake-3.16.9-Linux-x86_64.sh
+mkdir cmake-3.16.9-linux-x86_64
+./cmake-3.16.9-Linux-x86_64.sh --skip-license --prefix=cmake-3.16.9-linux-x86_64
+cmake-3.16.9-linux-x86_64/bin/cmake --configure .
+cmake-3.16.9-linux-x86_64/bin/cmake --build .
 cd ..
 
 
